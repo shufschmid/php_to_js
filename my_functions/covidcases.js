a036b5cf021ff77f8c4fa34064b41e8a13a7bf4d
shufschmid <shufschmid@gmail.com>
2020-12-27 21:48:58 +0100

    now with real data

--- my_functions/covidcases.js
+++ my_functions/covidcases.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch')
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://data.bs.ch/api/records/1.0/search/?dataset=100073&q=&sort=timestamp&facet=timestamp";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data.joke
+      body: data.records
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
